a7ffb41f35730377f4a967143b70201512f567aa
SAM's pc <Waliur Rafiq Sami>
2025-06-22 15:47:21 +0600

    1040

--- BEGINNER/s_1038.cpp
+++ BEGINNER/s_1038.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
+    int x, y;
+    cin >> x >> y;
+
+    if (x == 1)
+        printf("Total: R$ %.2lf\n", 4.0 * y);
+    else if (x == 2)
+        printf("Total: R$ %.2lf\n", 4.50 * y);
+    else if (x == 3)
+        printf("Total: R$ %.2lf\n", 5.0 * y);
+    else if (x == 4)
+        printf("Total: R$ %.2lf\n", 2.0 * y);
+    else if (x == 5)
+        printf("Total: R$ %.2lf\n", 1.50 * y);
 
     return 0;
 }
